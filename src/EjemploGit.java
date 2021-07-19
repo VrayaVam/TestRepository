7eec5806aa8eb0fdf9327a8510fb528236ea1e2f
Lucye <lethiciass.r@gmail.com>
2021-07-19 08:11:10 -0500

    Variables agregadas

--- src/EjemploGit.java
+++ src/EjemploGit.java
@@ -4,7 +4,7 @@ public class EjemploGit {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Saludos mentecatos, stupid nerdies");
-
+		String name="VanVam";
 	}
 
 }
