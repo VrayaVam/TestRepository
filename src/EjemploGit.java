9af192be3905abdfc84b85aff1519022c9c44f72
Lucye <lethiciass.r@gmail.com>
2021-07-19 08:11:41 -0500

    Impresion de variable

--- src/EjemploGit.java
+++ src/EjemploGit.java
@@ -5,6 +5,7 @@ public class EjemploGit {
 		// TODO Auto-generated method stub
 		System.out.println("Saludos mentecatos, stupid nerdies");
 		String name="VanVam";
+		System.out.println(name);
 	}
 
 }
